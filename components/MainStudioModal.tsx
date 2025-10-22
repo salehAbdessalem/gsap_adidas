00d2bfaa584916e93bab8a03b9c2aab56b30dece
salehAbdessalem <salih.abdessalem@gmail.com>
2025-10-22 17:25:11 +0100

    Add lights & animations to shirts

--- components/MainStudioModal.tsx
+++ components/MainStudioModal.tsx
@@ -1,9 +1,11 @@
 import * as THREE from "three";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 import { useMainStudioTextures } from "@/lib/useTextures";
 import { createMaterials } from "@/lib/material";
 import { studioTextures } from "@/lib/textures";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type GLTFResult = {
   nodes: {
@@ -22,35 +24,93 @@ export function MainStudioModel() {
     THREE.MeshBasicMaterial
   >;
 
+  const shirts = [
+    {
+      position: [0.65, 0.7, -0.45] as [number, number, number],
+      rotation: [0, Math.PI / 9, 0] as [number, number, number],
+      geometry: nodes.Shirt_White.geometry,
+      material: mats.whiteShirt,
+      hoverdMat: mats.whiteStudio,
+      slug: "white",
+    },
+    {
+      position: [0, 0.7, 0] as [number, number, number],
+      rotation: [0, 0, 0] as [number, number, number],
+      geometry: nodes.Shirt_Sport.geometry,
+      material: mats.sportShirt,
+      hoverdMat: mats.redStudio,
+      slug: "sport",
+    },
+    {
+      position: [-0.65, 0.7, -0.45] as [number, number, number],
+      rotation: [0, -Math.PI / 9, 0] as [number, number, number],
+      geometry: nodes.Shirt_Gray.geometry,
+      material: mats.grayShirt,
+      hoverdMat: mats.grayStudio,
+      slug: "gray",
+    },
+  ];
+
+  const [envMaterial, setEnvMaterial] = useState<THREE.MeshBasicMaterial>(
+    mats.defaultStudio
+  );
+
+  const meshRefs = useRef<(THREE.Mesh | null)[]>([]);
+  const tlRefs = useRef<GSAPTimeline[]>([]);
+  useGSAP(() => {
+    if (!meshRefs.current) return;
+    meshRefs.current.forEach((mesh, i) => {
+      if (!mesh) return;
+      tlRefs.current[i] = gsap
+        .timeline({ paused: true })
+        .to(mesh.rotation, { y: 0, duration: 1, ease: "power1.inOut" })
+        .to(mesh.scale, {
+          x: 1.05,
+          y: 1.05,
+          z: 1.05,
+          duration: 1,
+          ease: "power1.inOut",
+        }, "<");
+    });
+  });
+
+  function enterHandler(index: number, material: THREE.MeshBasicMaterial) {
+    document.body.style.cursor = "pointer";
+    setEnvMaterial(material);
+    tlRefs.current[index].play()
+  }
+  function leaveHandler(index: number) {
+    document.body.style.cursor = "auto";
+    tlRefs.current[index].reverse()
+  }
   return (
     <group dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Environment.geometry}
-        material={mats.defaultStudio}
+        material={envMaterial}
       />
-      <mesh
-        geometry={nodes.Shirt_White.geometry}
-        position={[0.65, 0.7, -0.45]}
-        rotation={[0, Math.PI / 9, 0]}
-        material={mats.whiteShirt}
-      />
-      <mesh
-        geometry={nodes.Shirt_Sport.geometry}
-        position={[0, 0.7, 0]}
-        material={mats.sportShirt}
-      />
-      <mesh
-        geometry={nodes.Shirt_Gray.geometry}
-        position={[-0.65, 0.7, -0.45]}
-        rotation={[0, -Math.PI / 9, 0]}
-        material={mats.grayShirt}
-      />
+      {shirts.map((shirt, i) => (
+        <mesh
+          key={i}
+          ref={(m) => {
+            if (!m) return;
+            meshRefs.current[i] = m;
+          }}
+          geometry={shirt.geometry}
+          material={shirt.material}
+          position={shirt.position}
+          rotation={shirt.rotation}
+          onPointerEnter={() => enterHandler(i, shirt.hoverdMat)}
+          onPointerLeave={() => leaveHandler(i)}
+        />
+      ))}
       <mesh
         geometry={nodes.Hitbox.geometry}
         scale={[2.52, 1, 1]}
         visible={false}
+        onPointerLeave={() => setEnvMaterial(mats.defaultStudio)}
       />
     </group>
   );
--- components/ViewCanvas.tsx
+++ components/ViewCanvas.tsx
@@ -2,12 +2,14 @@
 import { Canvas } from "@react-three/fiber";
 import { MainStudioModel } from "./MainStudioModal";
 import { OrbitControls } from "@react-three/drei";
+import Rig from "./Rig";
 
 const ViewCanvas = () => {
   return (
-    <Canvas style={{ position: "fixed", inset: 0, overflow: "hidden" }}>
+    <Canvas style={{ position: "fixed", inset: 0, overflow: "hidden" }} camera={{position: [0, 0.7, 3], fov: 30}}>
       <MainStudioModel />
-      <OrbitControls />
+      {/* <OrbitControls /> */}
+      <Rig />
     </Canvas>
   );
 };
